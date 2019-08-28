1dd6ed96c3a4a8bf5134b69903874c0e30cc04c1
arioscen <arioscen@gmail.com>
2019-08-28 19:07:03 +0800

    add migrate to update.sh

--- script/update.sh
+++ script/update.sh
@@ -3,3 +3,4 @@ SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_FOLDER_PATH=$(dirname "$SCRIPT_PATH")
 LARAVEL_PATH=$(dirname "$SCRIPT_FOLDER_PATH")
 docker run --rm -i -t -v $LARAVEL_PATH:/app -v /tmp:/tmp composer install
+docker-compose exec php php /usr/share/nginx/html/laravel/artisan migrate
