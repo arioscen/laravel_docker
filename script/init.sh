92cf6993280638303523c658d34cf3b144314ecc
arioscen <arioscen@gmail.com>
2019-08-29 18:18:22 +0800

    更新init.sh

--- script/init.sh
+++ script/init.sh
@@ -2,9 +2,10 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_FOLDER_PATH=$(dirname "$SCRIPT_PATH")
 LARAVEL_PATH=$(dirname "$SCRIPT_FOLDER_PATH")
-sh $SCRIPT_FOLDER_PATH/update.sh
 cd $LARAVEL_PATH
-chmod 777 -R storage
 cp -f .env.example .env
+chmod 777 -R storage
+docker run --rm -i -t -v $LARAVEL_PATH:/app -v /tmp:/tmp composer install
 docker-compose up -d
 docker-compose exec php php /usr/share/nginx/html/laravel/artisan key:generate
+sh $SCRIPT_FOLDER_PATH/update.sh
