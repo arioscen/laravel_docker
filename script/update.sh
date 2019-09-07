58a84200b2830ed22e9dbb1e8d6159eecedddf12
arioscen <arioscen@gmail.com>
2019-09-08 02:26:55 +0800

    fix tty error

--- script/update.sh
+++ script/update.sh
@@ -3,10 +3,10 @@ SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_FOLDER_PATH=$(dirname "$SCRIPT_PATH")
 LARAVEL_PATH=$(dirname "$SCRIPT_FOLDER_PATH")
 cd $LARAVEL_PATH
-git checkout branch master
-git pull -u origin master
+sudo git checkout master
+sudo git pull -u origin master
 sudo docker run --rm -v $LARAVEL_PATH:/app -v /tmp:/tmp composer install
-sudo docker-compose exec php php /usr/share/nginx/html/laravel/artisan migrate
-sudo docker-compose exec php php /usr/share/nginx/html/laravel/artisan db:seed
+sudo docker-compose exec -T php php /usr/share/nginx/html/laravel/artisan migrate
+sudo docker-compose exec -T php php /usr/share/nginx/html/laravel/artisan db:seed
 sudo docker run --rm -v $LARAVEL_PATH/:/home/node/app -w /home/node/app node npm install
 sudo docker run --rm -v $LARAVEL_PATH/:/home/node/app -w /home/node/app node npm run production
